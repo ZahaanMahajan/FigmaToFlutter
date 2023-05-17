fdc0c67764a0f3f062c8d906c7b5587565cbc107
Zahaan <zahaanjavaid032@gmail.com>
2023-05-17 15:56:14 +0530

    rat ya tavkeer

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
 import 'package:figma/screen/camera/camera_screen.dart';
+import 'package:figma/screen/financial_goal/financial_goal_screen.dart';
+import 'package:figma/screen/test_result/test_result_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CameraScreen(),
+      home: FinancialGoalScreen(),
     );
   }
 }
